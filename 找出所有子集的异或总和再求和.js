fdc72867960a72bdbac6c499cd24d6920c166837
吃拿卡要 <810921727@qq.com>
2021-08-17 17:07:41 +0800

    电话号码的字母组合

---
+++ 找出所有子集的异或总和再求和.js
@@ -0,0 +1,8 @@
+var subsetXORSum = function (nums) {
+  const ret = [0]
+  nums.forEach(i => ret.forEach(s => ret.push(i ^ s)))
+  console.log(ret)
+  return ret.reduce((r, i) => r + i, 0)
+}
+
+console.log(subsetXORSum([5, 1, 6]));
