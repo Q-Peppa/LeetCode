a93fe441e581b3da24a484d6fc40c25a79d1005e
吃拿卡要 <810921727@qq.com>
2021-10-24 17:10:06 +0800

    大礼包

---
+++ 求众数2.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var majorityElement = function (nums) {
+  const cnt = new Map()
+  const ans = new Set()
+
+  let target = Math.floor(nums.length / 3);
+  if (nums.length <= 2) {
+    return [...new Set(nums)]
+  }
+  for (let num of nums) {
+    cnt.set(num, (cnt.get(num) ?? 0) + 1)
+    if (cnt.get(num) > target)
+      ans.add(num);
+  }
+  return [...ans]
+};
+console.log(majorityElement([3, 2, 3]));
+console.log(majorityElement([1]));
+console.log(majorityElement([1, 1, 1, 3, 3, 2, 2, 2]));
