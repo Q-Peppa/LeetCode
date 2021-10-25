abf1dec3b798bc7d400d38c7add7bee1b727b813
吃拿卡要 <810921727@qq.com>
2021-10-25 14:48:55 +0800

    D:\work\LeetCode\按位与.js

---
+++ 按位与.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var rangeBitwiseAnd = function (left, right) {
+    while(right > left){
+        right &= (right-1)
+    }
+    return right
+};
+
+console.log(rangeBitwiseAnd(5,7))
+console.log(rangeBitwiseAnd(0, 0))
+console.log(rangeBitwiseAnd(1,2147483647))
+console.log(rangeBitwiseAnd(20000,
+    2147483647))
