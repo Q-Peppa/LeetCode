178a0d67256f2000fe73f9c565ae85049d9a1abf
Q-Peppa <810921727@qq.com>
2022-06-25 00:13:31 +0800

    demo

--- src/829.js
+++ src/829.js
@@ -1,39 +1,18 @@
-const _ = {
-    sum(s){
-
-        return s.reduce((a,b)=>a+b)
+//https://leetcode.cn/problems/find-largest-value-in-each-tree-row/
+var largestValues = function (root) {
+    if (!root) return [];
+    let res = [];
+    let queue = [root];
+    while (queue.length) {
+        let len = queue.length;
+        let max = -Infinity;
+        for (let i = 0; i < len; i++) {
+            let node = queue.shift();
+            max = Math.max(max, node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        res.push(max);
     }
+    return res;
 }
-/**
- * @param {number} n
- * @return {number}
- */
- var consecutiveNumbersSum = function(n) {
-    if(n <= 3) return n ;
-    let ans = 0
-    const dfs = (sum) =>  {
-        if(sum.length === 0) {
-            return ;
-        }
-        const nowSum = _.sum(sum);
-        const len = sum.length;
-        if(nowSum === n　||　sum[len -1] > n) {
-
-           ans++
-           sum.shift();
-        }else if(nowSum < n) {
-            sum.push(sum[len-1] + 1)
-        }else{
-            sum.shift()
-        }
-        dfs(sum)
-    }
-
-
-    dfs([1])
-    return ans;
-};
-
-// const a = consecutiveNumbersSum(15)
-const ans = consecutiveNumbersSum(10e2);
-console.log( ans);
