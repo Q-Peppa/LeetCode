9a32a68ff77dabe11b97d587035085fd971771ef
Peppa <810921727@qq.com>
2022-12-07 23:29:18 +0800

    demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   root: true,
-  extends: ['airbnb', 'prettier'],
+  extends: ['recommond', 'prettier'],
 };
--- test.lua
+++
@@ -1,9 +0,0 @@
-function maxValue(a, b)
-    return a > b and a or b
-end
-
-tab1 = { key1 = "val1", key2 = "val2", "val1233" }
-
-for k, v in inpairs(tab1) do
-    print(k .. "=" .. v)
-end
