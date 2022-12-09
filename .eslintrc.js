ecc5583d49d36099ca0f0393fbf640a9dc9b040c
Peppa <810921727@qq.com>
2022-12-10 00:23:36 +0800

    feat: eslint add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  root: true,
-  extends: ['prettier'],
+  plugins: ['prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
