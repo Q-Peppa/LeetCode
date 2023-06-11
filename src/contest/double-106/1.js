d6c16d4d32980f90d973ca35c55c2a035e662103
Peppa <810921727@qq.com>
2023-06-12 00:22:39 +0800

    add: 双周赛106

---
+++ src/contest/double-106/1.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isFascinating = function (n) {
+  let str = n.toString() + (n * 2).toString() + (n * 3).toString();
+  if (str.length !== 9) return false;
+  for (let i = 1; i <= 9; i++) {
+    if (!str.includes(i.toString())) return false;
+  }
+  return true;
+};
