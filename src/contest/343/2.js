4a41bf5c59e553cd72fd7da2df75b89a80408c3d
Peppa <54980372+Q-Peppa@users.noreply.github.com>
2023-07-27 23:09:32 +0800

    feat(name) : 恢复题目的命名

--- src/contest/343/2.js
+++ src/contest/343/2.js
@@ -1,36 +1,36 @@
 class Pair {
-  x: number;
-  y: number;
-  constructor(public x1: number, public y1: number) {
+  constructor(x1, y1) {
+    this.x1 = x1;
+    this.y1 = y1;
     this.x = x1;
     this.y = y1;
   }
-  get first(): number {
+  get first() {
     return this.x;
   }
-  get second(): number {
+  get second() {
     return this.y;
   }
-  swap(): Pair {
+  swap() {
     return new Pair(this.y, this.x);
   }
-  toArray(): number[] {
+  toArray() {
     return [this.x, this.y];
   }
 }
-function firstCompleteIndex(arr: number[], mat: number[][]): number {
+function firstCompleteIndex(arr, mat) {
   const NOT_FOUND = -1;
   const m = mat.length,
     n = mat[0].length;
-  const map = new Map<number, Pair>();
+  const map = new Map();
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; ++j) {
       const val = mat[i][j];
       map.set(val, new Pair(i, j));
     }
   }
-  const col = new Array<number>(n).fill(0);
-  const row = new Array<number>(m).fill(0);
+  const col = new Array(n).fill(0);
+  const row = new Array(m).fill(0);
   for (let i = 0; i < arr.length; ++i) {
     const val = arr[i];
     const p = map.get(val);
