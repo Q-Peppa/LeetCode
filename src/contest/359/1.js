925866d91b5095bcb2664ee22b59ddf11c1d2c5d
Peppa <54980372+Q-Peppa@users.noreply.github.com>
2023-08-20 11:52:46 +0800

    feat(357) : 周赛357

---
+++ src/contest/359/1.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {boolean}
+ */
+var isAcronym = function (words, s) {
+  return words.map((e) => e[0]).join('') === s;
+};
+console.log(isAcronym(['an', 'apple'], 'aa'));
