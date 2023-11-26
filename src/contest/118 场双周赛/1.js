95d4e40879af7de08a1be0524a715c50a3caf55c
Peppa <54980372+Q-Peppa@users.noreply.github.com>
2023-11-26 12:18:27 +0800

    feat 周赛

---
+++ src/contest/118 场双周赛/1.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function (words, x) {
+  const res = [];
+  let index = 0;
+  for (const e of words) {
+    if (e.includes(x)) {
+      res.push(index);
+    }
+    index++;
+  }
+  return res;
+};
---
+++ src/contest/373 场周赛/1.js
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var beautifulSubstrings = function (s, k) {
+  const isVowel = (c) => {
+    return c === 'a' || c === 'e' || c === 'i' || c === 'o' || c === 'u';
+  };
+
+  let ans = 0;
+  for (let i = 0; i < s.length; i++) {
+    let voNum = isVowel(s[i]) ? 1 : 0;
+    let coNum = isVowel(s[i]) ? 0 : 1;
+    for (let j = i + 1; j < s.length; j++) {
+      if (isVowel(s[j])) {
+        voNum++;
+      } else {
+        coNum++;
+      }
+      if (voNum === coNum && (voNum * coNum) % k === 0) {
+        ans++;
+      }
+    }
+  }
+  return ans;
+};
+console.time('112');
+console.log(
+  112 ===
+    beautifulSubstrings(
+      'auooubignuoyoaiyoetrezuuucuocemaeefldjqwoioizoyoputaaixrasvuseichoikpsjgeevbwebeoxuejagyoxiuqhgmoapiketuzjnmaaoamceejuujguqafquzaqwhnuiuiofuebaiiwiavetqiiearkuoovukwouauuhipauiguaadojcaaeiicbwaozouihusgzalacbiqbffsdeousaecsvqudjouixoojettoiizaaumeforosgiiiewiouuoouoaqfooyhgacawpeeeowpouhieogstoghvutljmuuvkeenliyyueaeeiooiwhiesamwaowmoehhshedslidnosbnojjrmdoijtaebavgkaizeaeeolcgooyapaofeiuugfzoeuwydouiuoqnaexruhzoyobhujwupoiipaaqwokioooaodfufouuoexcobliunbirebbxqlsuevtntdreooeueedfraqlcoreeilqoeuyczxnadiejvehieiebiykkvpiijeozignatgevuauiihioorqrnlhmzmyoiasiuajlrerinuuxoudbweeoiaajibtikko',
+      135,
+    ),
+);
+console.timeEnd('112');
