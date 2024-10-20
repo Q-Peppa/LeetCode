10f51b2e9851baec81f53c54985e51146d91e43d
吃拿卡要 <810921727@qq.com>
2024-10-20 20:15:54 +0800

    feat(420) : 周赛420

---
+++ src/contest/420/1.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} target
+ * @return {string[]}
+ */
+var stringSequence = function (target) {
+  const res = [];
+  const alpha = new Array(26)
+    .fill(0)
+    .map((_, i) => String.fromCharCode(i + 'a'.charCodeAt(0)));
+
+  const ok = (prefix, cur) => {
+    const index = alpha.indexOf(cur);
+    const temp = [];
+    for (let i = 0; i <= index; i++) {
+      temp.push(prefix + alpha[i]);
+    }
+    return temp;
+  };
+  for (let index = 0; index < target.length; index++) {
+    const cur = target[index];
+    const pre = target.slice(0, index);
+    res.push(ok(pre, cur));
+  }
+  return res.flat();
+};
---
+++ src/contest/420/2.js
@@ -0,0 +1,28 @@
+/**
+ * 100369. 字符至少出现 K 次的子字符串 I
+ * 给你一个字符串 s 和一个整数 k，在 s 的所有子字符串中，请你统计并返回 至少有一个 字符 至少出现 k 次的子字符串总数。
+ * 子字符串 是字符串中的一个连续、 非空 的字符序列。
+ * 1 <= s.length <= 3000
+ * 1 <= k <= s.length
+ */
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfSubstrings = function (s, k) {
+  let res = 0,
+    left = 0;
+  let tracker = {};
+  for (let i = 0; i < s.length; i++) {
+    const cur = s[i];
+    tracker[cur] = (tracker[cur] || 0) + 1;
+    while (tracker[cur] >= k) {
+      tracker[s[left]]--;
+      left++;
+    }
+    res += left;
+  }
+  return res;
+};
