7e923e23bdf7ba6d2401ac6204f96b0e409eef20
Peppa <54980372+Q-Peppa@users.noreply.github.com>
2025-09-02 23:43:57 +0800

    Update 2517.js

--- src/2517.js
+++ src/2517.js
@@ -1,22 +1,25 @@
-/**
- * @param {number[]} price
- * @param {number} k
- * @return {number}
- */
-var maximumTastiness = function (price, k) {
-  price.sort((a, b) => a - b);
-  if (k === 2) return price[price.length - 1] - price[0];
-  console.log(price);
-  let head = price[0];
-  let tail = price[price.length - 1];
-  let max = 0;
-  for (let i = 1; i < price.length - 1; i++) {
-    let temp = Math.min(price[i] - head, tail - price[i]);
-    if (temp > max) max = temp;
-  }
-  return max;
+var maximumTastiness = function(price, k) {
+    price.sort((a, b) => a - b);
+    let left = 0, right = price[price.length - 1] - price[0];
+    while (left < right) {
+        const mid = Math.floor((left + right + 1) / 2);
+        if (check(price, k, mid)) {
+            left = mid;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+
+const check = (price, k, tastiness) => {
+    let prev = -Number.MAX_VALUE / 2;
+    let cnt = 0;
+    for (const p of price) {
+        if (p - prev >= tastiness) {
+            cnt++;
+            prev = p;
+        }
+    }
+    return cnt >= k;
 };
-// console.log(maximumTastiness([13, 5, 1, 8, 21, 2], 3));
-// console.log(maximumTastiness([7, 7, 7, 7], 2));
-// console.log(maximumTastiness([1, 3, 1], 2));
-console.log(maximumTastiness([34, 116, 83, 15, 150, 56, 69, 42, 26], 6));
