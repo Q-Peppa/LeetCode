8a5d0328004dcf6c82bed28fa65421a25e759a01
Peppa <54980372+Q-Peppa@users.noreply.github.com>
2026-01-19 01:04:11 +0800

    feat(src): add regular expression matching solution (LeetCode #10)

---
+++ src/10.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function (s, p) {
+  const m = s.length;
+  const n = p.length;
+  const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(false));
+  dp[0][0] = true;
+
+  for (let j = 2; j <= n; j += 2) {
+    if (p[j - 1] === '*') {
+      dp[0][j] = dp[0][j - 2];
+    }
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (p[j - 1] === '*') {
+        dp[i][j] = dp[i][j - 2];
+        if (p[j - 2] === s[i - 1] || p[j - 2] === '.') {
+          dp[i][j] = dp[i][j] || dp[i - 1][j];
+        }
+      } else if (p[j - 1] === '.' || p[j - 1] === s[i - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      }
+    }
+  }
+
+  return dp[m][n];
+};
+
+console.log(isMatch('aa', 'a'), '1', 'ans is false');
+
+console.log(isMatch('aa', 'a*'), '2', 'ans is true');
+
+console.log(isMatch('ab', '.*'), '3', 'ans is true');
