488d2cb3f3d60ccaa4bc26f908275f05e6a23b99
Peppa <54980372+Q-Peppa@users.noreply.github.com>
2026-02-28 00:19:01 +0800

    feat: add 1404

--- src/1404.js
+++ src/1404.js
@@ -3,26 +3,22 @@
  * @return {number}
  */
 var numSteps = function (s) {
-  let steps = 0,
-    str = s.split('');
-  while (str.join('') !== '1') {
-    steps++;
-    if (str[str.length - 1] === '0') {
-      str.pop();
+  let steps = 0;
+  let carry = 0;
+  for (let i = s.length - 1; i > 0; i--) {
+    const bit = s[i] === '1' ? 1 : 0;
+    if (bit + carry === 1) {
+      steps += 2;
+      carry = 1;
     } else {
-      for (let i = str.length - 1; i >= 0; i--) {
-        if (str[i] === '1') {
-          str[i] = '0';
-          if (i === 0) {
-            str.unshift('1');
-            break;
-          }
-        } else {
-          str[i] = '1';
-          break;
-        }
-      }
+      steps += 1;
     }
   }
-  return steps;
+  return steps + carry;
 };
+
+console.log(numSteps('1'), '1', 'ans is 0');
+console.log(numSteps('10'), '2', 'ans is 1');
+console.log(numSteps('1101'), '3', 'ans is 6');
+console.log(numSteps('111'), '4', 'ans is 4');
+console.log(numSteps('101'), '5', 'ans is 5');
