457b7aad9e4eb5d920c02724796dfcb614df363d
Peppa <54980372+Q-Peppa@users.noreply.github.com>
2026-02-28 00:38:13 +0800

    fix(hooks): restage staged files after biome write

--- src/999999.js
+++ src/999999.js
@@ -1,6 +1,10 @@
 /**
-* @param {number[]} nums
-* @return {number}
-*/
-var testHook=function(nums){let x=1+2;console.log( "bad" ) ;return nums.length+x}
-console.log(testHook([1,2,3]))
+ * @param {number[]} nums
+ * @return {number}
+ */
+var testHook = function (nums) {
+  let x = 1 + 2;
+  console.log('bad2');
+  return nums.length + x;
+};
+console.log(testHook([1, 2, 3]));
